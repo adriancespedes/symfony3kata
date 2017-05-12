4f58719d5f4a8a80ac0c4eeea71bfe7c67a984cc
Adrian Cespedes <adrian@enalquiler.com>
2017-05-12 10:13:55 +0200

    show Json response

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -44,6 +44,7 @@ class BlogController extends Controller
         if ($name !== 'Toni' && $name !== 'Siscu') {
             throw $this->createNotFoundException('No es Toni o Siscu');
         }
+
         return new Response('Hola '.$name);
     }
 
@@ -55,5 +56,53 @@ class BlogController extends Controller
     public function helloToniSiscoForbiddenAction(String $name)
     {
         return new Response('Hola '.$name);
+    }
+
+    /**
+     * @Route("/swapi")
+     * @return Response
+     */
+    public function swapiAction()
+    {
+        $swapiResponse = [
+            'birth_year' => '19BBY',
+            'created' => '2014-12-09T13:50:51.644000Z',
+            'edited' => '2014-12-20T21:17:56.891000Z',
+            'eye_color' => 'blue',
+            'films' =>
+                array(
+                    0 => 'http://swapi.co/api/films/2/',
+                    1 => 'http://swapi.co/api/films/6/',
+                    2 => 'http://swapi.co/api/films/3/',
+                    3 => 'http://swapi.co/api/films/1/',
+                    4 => 'http://swapi.co/api/films/7/',
+                ),
+            'gender' => 'male',
+            'hair_color' => 'blond',
+            'height' => '172',
+            'homeworld' => 'http://swapi.co/api/planets/1/',
+            'mass' => '77',
+            'name' => 'Luke Skywalker',
+            'skin_color' => 'fair',
+            'species' =>
+                array(
+                    0 => 'http://swapi.co/api/species/1/',
+                ),
+            'starships' =>
+                array(
+                    0 => 'http://swapi.co/api/starships/12/',
+                    1 => 'http://swapi.co/api/starships/22/',
+                ),
+            'url' => 'http://swapi.co/api/people/1/',
+            'vehicles' =>
+                array(
+                    0 => 'http://swapi.co/api/vehicles/14/',
+                    1 => 'http://swapi.co/api/vehicles/30/',
+                ),
+        ];
+
+
+
+        return $this->json($swapiResponse);
     }
 }
